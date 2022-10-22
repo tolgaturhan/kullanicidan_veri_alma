c7f096248df6f1ab96fb03d7f62acb36b0bfba77
Tolga Turhan <tolgaturhan@live.com>
2022-10-22 10:14:43 +0300

    v0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int a;
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Lütfen Sayı Giriniz: ");
+        a = input.nextInt();
+        System.out.println(a);
+
+        System.out.print("Lütfen Sayı Giriniz: ");
+        int b = input.nextInt(); // Buda farklı bir kullanım değer oluşturup bu değere girdi isteyebiliriz
+        System.out.println(b);
+
 
     }
 }
